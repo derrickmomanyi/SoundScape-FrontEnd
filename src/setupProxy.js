ede070cfa6698e06805109730aab3e30f983335a
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-04-01 17:21:02 +0300

    Added proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://soundscape-development.onrender.com',
+      target: 'https://railsapp-soundscape.onrender.com',
       changeOrigin: true,
     })
   );
